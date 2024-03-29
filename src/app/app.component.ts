992a76e034435aad00f0b8a5749d83f83b7faf7d
Mithun Pes <mithunpes180@gmail.com>
2024-03-29 10:56:41 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+//  standalone:true,
+  //imports: [FormsModule, CommonModule],
 })
-export class AppComponent {
-  title = 'task4';
+export class AppComponent  {
+ 
+
+
 }
---
+++ src/app/table/table.component.ts
@@ -0,0 +1,51 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-table',
+  templateUrl: './table.component.html',
+  styleUrl: './table.component.scss',
+  // standalone:true,
+  // imports: [FormsModule, CommonModule],
+})
+export class TableComponent implements OnInit{
+  public ngvals= false;
+  public red="red";
+  public blue="blue";
+  public green="green";
+  public aqua="aqua";
+ public columns=["red","blue","yellow","orange"];
+  public value1="";
+  shows(){
+    this.ngvals=true;
+  }
+  hides(){
+    this.ngvals=false;
+  }
+  chng(){
+    for(let i=0;i<this.columns.length;i++)
+    {
+      if(this.columns[i]=="red")
+      {
+        this.columns[i]="reds";
+      }
+      else if(this.columns[i]=="blue")
+      {
+        this.columns[i]="blues";
+      }
+      else if(this.columns[i]=="yellow")
+      {
+        this.columns[i]="yellows";
+      }
+      else if(this.columns[i]=="orange")
+      {
+        this.columns[i]="oranges";
+      }
+    }
+  }
+
+  constructor(){}
+  ngOnInit(): void {}
+
+}
